f74e350de99af4b8bd0652c0ef2e79528a8a465e
pandrei7 <preda.andrei174@gmail.com>
2020-08-02 13:45:44 +0300

    Add config.fish

---
+++ config.fish
@@ -0,0 +1,17 @@
+# Customizations for the prompt
+set -g pure_symbol_prompt 'λ'
+set -g pure_color_prompt_on_success    '98971A'
+set -g pure_color_prompt_on_error      'CC241D'
+set -g pure_color_current_directory    '458588'
+set -g pure_color_command_duration     'D79921'
+set -g pure_color_git_branch           'A89984'
+set -g pure_color_git_dirty            'D65D0E'
+set -g pure_color_git_unpulled_commits 'FE8019'
+set -g pure_color_git_unpushed_commits 'FE8019'
+
+# Custom colors
+set -g fish_color_command        '98971A' '--bold'
+set -g fish_color_error          'CC241D'
+set -g fish_color_param          '458588'
+set -g fish_color_autosuggestion '665C54'
+set -g fish_color_valid_path
