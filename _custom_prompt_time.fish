47da4865dd02b70a7e88f2d528d088ec981fab1a
pandrei7 <preda.andrei174@gmail.com>
2020-08-02 13:43:25 +0300

    Change fish prompt time color

--- _custom_prompt_time.fish
+++ _custom_prompt_time.fish
@@ -2,7 +2,7 @@
 # used to customize the "Pure" fish prompt.
 function _custom_prompt_time
     set --local time (date +%H:%M)
-    set --local time_color (_pure_set_color $pure_color_command_duration)
+    set --local time_color (_pure_set_color 'FABD2F')
 
     echo "$time_color$time"
 end
