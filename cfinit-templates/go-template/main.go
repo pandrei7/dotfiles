8b4a67deacb10fbb70057b0e19d00edf405cf739
pandrei7 <preda.andrei174@gmail.com>
2021-07-03 13:46:02 +0300

    Update cfinit templates

---
+++ cfinit-templates/go-template/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+type Scanner struct {
+	*bufio.Scanner
+}
+
+func NewWordScanner(reader io.Reader) *Scanner {
+	scanner := bufio.NewScanner(reader)
+	scanner.Split(bufio.ScanWords)
+	return &Scanner{scanner}
+}
+
+func (s *Scanner) Word() string {
+	s.Scan()
+	return s.Text()
+}
+
+func (s *Scanner) Int() int {
+	s.Scan()
+	n, _ := strconv.Atoi(s.Text())
+	return n
+}
+
+func (s *Scanner) Int64() int64 {
+	s.Scan()
+	n, _ := strconv.ParseInt(s.Text(), 10, 64)
+	return n
+}
+
+func (s *Scanner) Ints(count int) []int {
+	ints := make([]int, count)
+	for i := range ints {
+		ints[i] = s.Int()
+	}
+	return ints
+}
+
+func (s *Scanner) Int64s(count int) []int64 {
+	int64s := make([]int64, count)
+	for i := range int64s {
+		int64s[i] = s.Int64()
+	}
+	return int64s
+}
+
+func main() {
+	stdin := NewWordScanner(os.Stdin)
+}
