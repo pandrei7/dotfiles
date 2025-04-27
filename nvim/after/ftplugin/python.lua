d7b3aa78cf24643656b6a680409bd603d69dd978
Andrei Preda <preda.andrei174@gmail.com>
2025-04-27 19:57:00 +0300

    Format Python imports with Isort instead of Pyright

--- nvim/after/ftplugin/python.lua
+++ nvim/after/ftplugin/python.lua
@@ -1,5 +1,5 @@
 -- Python needs extra work for formatting, because it's "special".
 vim.keymap.set("n", "<M-I>", function()
-  vim.cmd("PyrightOrganizeImports")
   vim.cmd("Black")
+  vim.cmd("Isort")
 end, { buffer = 0 })
