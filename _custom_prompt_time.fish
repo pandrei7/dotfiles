7be20de5b36ee91df864eae58d255076d1208f47
Andrei Preda <preda.andrei174@gmail.com>
2026-01-03 18:39:17 +0200

    Some color fix for Fish prompt

--- _custom_prompt_time.fish
+++ _custom_prompt_time.fish
@@ -1,8 +1,11 @@
 # A function which displays the current time,
 # used to customize the "Pure" fish prompt.
 function _custom_prompt_time
+    set --local original_color $color
+
     set --local time (date +%H:%M)
     set --local time_color (_pure_set_color 'FABD2F')
 
     echo "$time_color$time"
+    echo "$original_color"
 end
